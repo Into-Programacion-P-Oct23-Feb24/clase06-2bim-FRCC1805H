e87868be24599c18fcf74b06d2926e4c6195eb15
fabian <frcanar2@utpl.edu.ec>
2024-01-17 16:50:59 -0500

    clase06

--- Ejemplos/src/paquete1/Principal.java
+++ Ejemplos/src/paquete1/Principal.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package paquetedos;
+package paquete1;
 
 
-import paqueteuno.Operador;
+import paquete2.Operador;
 
 public class Principal {
     
